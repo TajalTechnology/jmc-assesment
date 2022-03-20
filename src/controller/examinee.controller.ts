997c26f11854dd7ba997dc383f97067df6c3ddd6
TajalTechnology <mrpresident1189@gmail.com>
2022-03-20 18:37:07 +0600

    implementing file upload package

--- src/controller/examinee.controller.ts
+++ src/controller/examinee.controller.ts
@@ -1,29 +1,29 @@
 import { Request, Response } from "express";
 import { testScores } from "../service/examinee.service";
-import { Num } from 'example-typescript-package';
-import { Strikeing } from 'upload-groots';
-
+// import { Num } from 'example-typescript-package';
+import { AWS } from "upload-groots";
 
 export async function myTestScoresHandler(req: Request, res: Response) {
-
   const myScores = await testScores();
   return res.status(200).json({ myScores });
-
-};
+}
 export async function myTest(req: Request, res: Response) {
   let file = req.file;
-  const strikeing = new Strikeing();
+  const aws = new AWS();
 
-  const accessKeyId = 'AKIA5UQZIKYENZREEJFF';
-  const secretAccessKey = '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';
-  const region = 'ap-southeast-1';
-  const bucketName = 'greeho-img';
+  const accessKeyId = "AKIA5UQZIKYENZREEJFF";
+  const secretAccessKey = "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";
+  const region = "ap-southeast-1";
+  const bucketName = "greeho-img";
 
   const credential = {
-    accessKeyId, secretAccessKey, region, bucketName
-  }
+    accessKeyId,
+    secretAccessKey,
+    region,
+    bucketName,
+  };
 
-  const credentials = await strikeing.play(credential, file);
+  const credentials = await aws.uploadFile(credential, file);
 
   console.log(credentials);
-};
+}
